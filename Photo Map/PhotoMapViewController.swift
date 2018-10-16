e57339d4c676bfc6519a079e65b5cff5fddf7c8f
Jesus Andres Bernal Lopez <jabernall24@gmail.com>
2018-10-15 21:07:29 -0700

    finished project and uploaded gif

--- Photo Map/PhotoMapViewController.swift
+++ Photo Map/PhotoMapViewController.swift
@@ -14,11 +14,15 @@ class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate,
     @IBOutlet weak var mapView: MKMapView!
     let sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667), MKCoordinateSpanMake(0.1, 0.1))
     var chosenImage: UIImage!
+    var resizeRenderImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 45, height: 45))//CGRectMake(0, 0, 45, 45))
+    let rightAccesoryButton = UIButton(type: .detailDisclosure)
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         mapView.setRegion(sfRegion, animated: false)
+        mapView.delegate = self
     }
     
     func locationsPickedLocation(controller: LocationsViewController, latitude: NSNumber, longitude: NSNumber) {
@@ -27,23 +31,41 @@ class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate,
         
         let annotation = MKPointAnnotation()
         annotation.coordinate = locationCoordinate
-        annotation.title = "Picture!"
+        annotation.title = String(describing: latitude)
+        
         mapView.addAnnotation(annotation)
+        rightAccesoryButton.addTarget(self, action: #selector(onRightAccesoryButton), for: .touchUpInside)
     }
     
-    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+    @objc func onRightAccesoryButton(){
+        self.performSegue(withIdentifier: "fullImageSegue", sender: self)
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         let reuseID = "myAnnotationView"
         var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseID)
-
+        
+        resizeRenderImageView.layer.borderColor = UIColor.white.cgColor
+        resizeRenderImageView.layer.borderWidth = 3.0
+        resizeRenderImageView.contentMode = UIViewContentMode.scaleAspectFill
+        resizeRenderImageView.image = chosenImage
+        
+        UIGraphicsBeginImageContext(resizeRenderImageView.frame.size)
+        resizeRenderImageView.layer.render(in: (UIGraphicsGetCurrentContext() as! CGContext!))
+        let thumbnail = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
         if (annotationView == nil) {
-            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseID)
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseID)
+//                MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseID)
             annotationView!.canShowCallout = true
             annotationView!.leftCalloutAccessoryView = UIImageView(frame: CGRect(x:0, y:0, width: 50, height:50))
+            annotationView?.rightCalloutAccessoryView = rightAccesoryButton
+            annotationView?.image = thumbnail
         }
 
         let imageView = annotationView?.leftCalloutAccessoryView as! UIImageView
-        imageView.image = UIImage(named: "camera")
-
+        imageView.image = thumbnail//resizeRenderImageView.image //UIImage(named: "camera")
         return annotationView
     }
     
@@ -75,6 +97,7 @@ class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate,
             self.present(alertController, animated: true)
         }else{
             vc.sourceType = .photoLibrary
+            self.present(vc, animated: true, completion: nil)
         }
         
     }
@@ -94,6 +117,9 @@ class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate,
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let locationsViewController = segue.destination as? LocationsViewController{
             locationsViewController.delegate = self as LocationViewControllerDelegate
+        }
+        if let fullImageViewController = segue.destination as? FullImageViewController{
+            fullImageViewController.photo = chosenImage
         }
     }
     
---
+++ PhotoAnnotation.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoAnnotation.swift
+//  Photo Map
+//
+//  Created by Jesus Andres Bernal Lopez on 10/15/18.
+//  Copyright © 2018 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+//class PhotoAnnotation: NSObject, MKAnnotation {
+//    var coordinate: CLLocationCoordinate2D = CLLocationCoordinate2DMake(0, 0)
+//    var photo: UIImage!
+//    
+//    var title: String? {
+//        return "\(coordinate.latitude)"
+//    }
+//}
